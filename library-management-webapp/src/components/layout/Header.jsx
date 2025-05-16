f613c0b58f73b06b9c7627155600c7fb4886d1e9
Madalena Andrade <mmadalenaandrade93@gmail.com>
2025-05-16 12:16:27 +0100

    Added link to Home on header

--- library-management-webapp/src/components/layout/Header.jsx
+++ library-management-webapp/src/components/layout/Header.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import libraryIcon from "../../images/library-icon.png";
 
 export default function Header() {
   return (
     <header>
-      <img src={libraryIcon} alt="Library logo" />
-      <h1>LibraryManagementApp</h1>
+      <Link to="/" className="header-link">
+        <img src={libraryIcon} alt="Library logo" />
+        <h1>LibraryManagementApp</h1>
+      </Link>
     </header>
   );
 }
